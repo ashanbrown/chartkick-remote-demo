bb6577c7490ac3688a61d4764a3c23b90285bb4a
Andrew S. Brown <andrew@dontfidget.com>
2014-06-30 00:06:48 -0700

    show meaningful times on demo and add controller code

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,9 @@
 class ChartsController < ApplicationController
   include Chartkick::Remote
-  chartkick_remote
+  chartkick_remote discrete: true
 
   def index
+    @start_time = (params[:start_time] || (Time.now.to_f * 1000)).to_i
+    params.merge!(start_time: @start_time)
   end
 end
